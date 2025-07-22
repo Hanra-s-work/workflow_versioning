7cef80d2964cc7090b51b2b4ba687317c85763e9
Henry LETELLIER <henry.letellier@epitech.eu>
2025-07-22 11:39:11 +0200

    [UPDATE] the launcher for the script

--- multi_in_one/python/scripts/compile/__main__.py
+++ multi_in_one/python/scripts/compile/__main__.py
@@ -4,4 +4,19 @@
 
 import sys
 
+# try:
+#     sys.path.append("..")
+#     from .. import globals
+# except ImportError:
+#     try:
+#         import globals
+#     except ImportError:
+#         print("Error: Unable to import globals module. Ensure you are running this script from the correct directory.")
+#         sys.exit(1)
+
+
 import compile as COMP
+
+if __name__ == "__main__":
+    compiler = COMP.Compile()
+    compiler.run()
